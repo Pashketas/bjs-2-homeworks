31a8b48a9e454f4bfd3fb800e21f37c98803cd0b
Pavel Perebeynosov <pashketas@gmail.com>
2022-10-08 22:04:50 +0300

    Solved Functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,13 +4,13 @@ function getArrayParams(arr) {
   min = arr[0];
   max = arr[0];
   sum = 0;
-  for (let i = 1; i < (arr.length + 1); i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] > max) {
       max = arr[i];
     } else if (arr[i] < min) {
       min = arr[i];
     }
-    sum = sum + arr[i - 1]; 
+    sum = sum + arr[i]; 
   }
   avg = Number(((sum / arr.length) .toFixed(2)));
   return { min: min, max: max, avg: avg };
@@ -18,23 +18,37 @@ function getArrayParams(arr) {
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+    };
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let dif = 0;
+  let min, max;
+  min = arr[0];
+  max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
+    }
+  }  
+  dif = Math.abs(max -min); 
+ return dif
 }
+
