b70c6eee49ecb072603edf077040caaf2b74d409
Pavel Perebeynosov <pashketas@gmail.com>
2022-10-11 23:16:37 +0300

    Solved HW №3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result;  
+  /*let arrSum = [arr1, arr2];
+    result = arrSum.every(comparingElemnts => arr1 === arr2);
+    не понимаю как использовать every в данных условиях задачи, вариант выше не работает */
+    if (arr1.toString() === arr2.toString()) {
+      result = true;
+    } else {
+      result = false;
+    }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter(positiveElement => positiveElement > 0).filter(devisibleElement => devisibleElement % 3 === 0).map((multipleElement) => multipleElement * 10);
   return resultArr; // array
 }
