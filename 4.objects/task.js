c5a53f739937726256f4ab9a34ab3eddec05e077
Pavel Perebeynosov <pashketas@gmail.com>
2022-10-16 12:45:44 +0300

    Solved HW №4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.geder = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined) { 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function(...marks) {
+  if(this.marks === undefined) { 
+    this.marks = [...marks];
+    } else {
+      this.marks.push(...marks);
+    }
+}
+
+Student.prototype.getAverage = function() {
+  let sum = this.marks.reduce((acc, item)=> acc + item, 0);
+  return (sum / this.marks.length);
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
